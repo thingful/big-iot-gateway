57164deedb6b46d4b355bc442c3a75cb6ec993ab
ChristianEspinoza <chespinoza@gmail.com>
2018-01-15 20:43:20 +0000

    restructure functionality

--- pkg/utils/functions.go
+++ pkg/utils/functions.go
@@ -1,7 +1,6 @@
 package utils
 
 import (
-	"encoding/json"
 	"strings"
 )
 
@@ -14,41 +13,4 @@ func GetOfferingIndex(id string, offerings []OfferingConfig) int {
 		}
 	}
 	return offeringIndex
-}
-
-// ConvertJSON takes pipe json and change to big-iot json depends on offerinConfig provide
-func ConvertJSON(pipeJson []byte, offering OfferingConfig) ([]byte, error) {
-
-	output := []map[string]interface{}{}
-
-	var m interface{}
-	err := json.Unmarshal(pipeJson, &m)
-	if err != nil {
-		return nil, err
-	}
-
-	j := m.([]interface{}) //type case to slice first
-
-	for _, member := range j {
-		pipeData := member.(map[string]interface{}) // then for each member, cast to map string interface
-
-		bigiotData := map[string]interface{}{} // make temporary var
-
-		for _, output := range offering.Outputs {
-			if val, ok := pipeData[output.PipeTerm]; ok { // find if the key exist, if it does assign it
-				bigiotData[output.BigiotName] = val
-			} else { // if it doesn't exist, assing default value
-				bigiotData[output.BigiotName] = ""
-			}
-		}
-
-		output = append(output, bigiotData)
-	}
-
-	s, err := json.Marshal(output)
-	if err != nil {
-		return nil, err
-	}
-
-	return s, nil
 }
