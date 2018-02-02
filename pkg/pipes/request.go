3caff93b94f91ccf258103fc93443af439634331
ChristianEspinoza <chespinoza@gmail.com>
2018-02-02 12:59:10 +0000

    refactor

--- pkg/pipes/request.go
+++ pkg/pipes/request.go
@@ -1,36 +1,21 @@
 package pipes
 
 import (
-	"errors"
 	"fmt"
-	"io/ioutil"
-	"net/http"
+
+	"github.com/go-resty/resty"
 )
 
 // MakeRequest calls at url using token for Authentication
 func MakeRequest(url string, token string) ([]byte, error) {
-
-	client := &http.Client{}
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	req.Header.Add("Authorization", "Bearer "+token)
-	resp, err := client.Do(req)
-	if err != nil || resp.StatusCode != 200 {
+	client := resty.New()
+	client.SetAuthToken(token)
+	resp, err := client.R().Get(url)
+	if err != nil || resp.StatusCode() != 200 {
 		if err == nil {
-			err = errors.New(fmt.Sprintf("Status Code: %d received", resp.StatusCode))
+			err = fmt.Errorf("status Code: %d received", resp.StatusCode())
 		}
 		return nil, err
 	}
-
-	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	return body, nil
+	return resp.Body(), nil
 }
